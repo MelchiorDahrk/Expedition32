d593afae65b03a57ac991168f2b7442f4d65962a
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2025-11-24 10:32:23 -0500

    renamed ESM

--- Developer Tools/merge_to_master.bat
+++ Developer Tools/merge_to_master.bat
@@ -1,5 +1,5 @@
 @echo off
-copy /y "..\\00 Core\\Exp32Master.esm" "Exp32Master.esm"
-.\\merge_to_master.exe %1 "Exp32Master.esm" --overwrite --remove-deleted --apply-moved-references
-move /y "Exp32Master.esm" "..\\00 Core\\Exp32Master.esm"
+copy /y "..\\00 Core\\TheGardenOfDreams.esm" "TheGardenOfDreams.esm"
+.\\merge_to_master.exe %1 "TheGardenOfDreams.esm" --overwrite --remove-deleted --apply-moved-references
+move /y "TheGardenOfDreams.esm" "..\\00 Core\\TheGardenOfDreams.esm"
 pause
