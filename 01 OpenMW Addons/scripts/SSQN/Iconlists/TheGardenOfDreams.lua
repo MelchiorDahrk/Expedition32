52c6965a67b57d2d8955921688dded2655539d6f
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2025-12-22 11:15:19 -0500

    Updated SSQN files to cover side quests

--- 01 OpenMW Addons/scripts/SSQN/Iconlists/TheGardenOfDreams.lua
+++ 01 OpenMW Addons/scripts/SSQN/Iconlists/TheGardenOfDreams.lua
@@ -2,5 +2,6 @@ local icons = {}
 -- Temple Master
 		icons.X32_MTM = "\\Icons\\SSQN\\MT.dds"
 		icons.x32 = "\\Icons\\x32\\q\\quest_thegarden.tga"
+		icons.x32_SideQ = "\\Icons\\x32\\q\\quest_thegarden.tga"
 
 return icons
