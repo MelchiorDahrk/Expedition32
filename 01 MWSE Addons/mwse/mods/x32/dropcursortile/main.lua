6865664354c664743b46ddd7ac6c453953261c94
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2025-12-23 16:47:18 -0500

    more melvin edits

---
+++ 01 MWSE Addons/mwse/mods/x32/dropcursortile/main.lua
@@ -0,0 +1,10 @@
+event.register("startGlobalScript", function(e)
+    if e.script.id == "x32_DropCursorTile_s" then
+        local tile = tes3ui.getCursorTile()
+        if tile then
+            tile.element:triggerEvent("mouseClick")
+            tes3ui.leaveMenuMode()
+        end
+        return false
+    end
+end)
