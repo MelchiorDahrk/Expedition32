d93f8cc6afa2c52cfd904e5dda47506fc993c374
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2025-12-31 18:55:03 -0500

    summon atronach WIP 2

--- 01 MWSE Addons/mwse/mods/x32/main.lua
+++ 01 MWSE Addons/mwse/mods/x32/main.lua
@@ -13,3 +13,24 @@ local function onInitialized()
     dofile("x32.dropcursortile")
 end
 event.register("initialized", onInitialized, { priority = 1000 })
+
+
+local function addSummoningEffects()
+    local framework = include("OperatorJack.MagickaExpanded")
+    if framework then
+        framework.effects.conjuration.createBasicSummoningEffect({
+            id = tes3.effect.summonNebulousAtronach,
+            name = "Summon Nebulous Atronach",
+            description = (
+                "This effect summons a ".. creatureName .." from The Nebula Between Worlds."..
+                    " It appears six feet in front of the caster and attacks any entity that attacks the caster until"..
+                    " the effect ends or the summoning is killed. At death, or when the effect ends, the summoning"..
+                    " disappears, returning to Oblivion. If summoned in town, the guards will attack you and the summoning on sight."
+            ),
+            baseCost = 18,
+            creatureId = "x32_Dae_AtronachNebulaS",
+            icon = "x32\\e\\b_tx_s_smmn_nebatro.tga"
+        })
+    end
+end
+event.register("magicEffectsResolved", addSummoningEffects)
--- 01 MWSE Addons/mwse/mods/x32/me/interop.lua
+++ 01 MWSE Addons/mwse/mods/x32/me/interop.lua
@@ -1,16 +1,15 @@
 local framework = include("OperatorJack.MagickaExpanded")
-debug.log(framework)
 tes3.setGlobal("x32_MagickaExpandedActive", framework ~= nil)
 
 if framework  then
-    require("x32.me.effects.x32SummonEffects")
+    tes3.claimSpellEffectId("summonNebulousAtronach", 7900)
 
     local function registerSpells()
         framework.spells.createBasicSpell({
             id = "x32_sp_SummonNebulousAtronach",
             name = "Summon Nebulous Atronach",
             effect = tes3.effect.summonNebulousAtronach,
-            range = tes3.effectRange.self,
+            rangeType = tes3.effectRange.self,
             duration = 60
         })
     end
