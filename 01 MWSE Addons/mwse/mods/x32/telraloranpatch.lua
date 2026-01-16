70946da4514fe741e4b8173095c477cab7758f2a
MwGek <76392007+MwGek@users.noreply.github.com>
2026-01-16 23:42:02 +0100

    Update telraloranpatch.lua

--- 01 MWSE Addons/mwse/mods/x32/telraloranpatch.lua
+++ 01 MWSE Addons/mwse/mods/x32/telraloranpatch.lua
@@ -1,5 +1,18 @@
-if tes3.isModActive("OAAB - Tel Raloran.ESP") then
-    tes3.setGlobal("x32_TelRaloranActive", 1)
-else
-    tes3.setGlobal("x32_TelRaloranActive", 0)
-end
+event.register("initialized", function()
+    -- Return if neither Garden of Dreams plugin is active
+    if not (
+        tes3.isModActive("TheGardenOfDreams.esp")
+        or tes3.isModActive("TheGardenOfDreams.esm")
+    ) then
+        return
+    end
+
+    -- Set global based on Tel Raloran plugins
+    tes3.setGlobal(
+        "x32_TelRaloranActive",
+        (
+            tes3.isModActive("OAAB - Tel Raloran.ESP")
+            or tes3.isModActive("OAAB - Tel Raloran - Immersive Tables.ESP")
+        ) and 1 or 0
+    )
+end)
